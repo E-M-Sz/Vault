21c78cc1f0e8e67dce1c5c67b3e8340f407c71ea
E-M-Sz <emsz.dev@gmail.com>
2017-03-22 15:57:48 +0100

    Basic Project Skeleton
    Added basic folder and layout structure for project

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 const express = require('express');
 const app = express();
 
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Use public folder for static files
+app.use(express.static(__dirname + '/public'));
+
+/* Routes */
+// Index
 app.get('/', function(req, res){
-  res.send('Welcome to my page...');
+  res.render('index', {nav: 'index'});
+});
+
+// Route2
+app.get('/route2', function(req, res){
+  res.render('route2', {nav: 'route2'});
 });
 
 app.listen(3000);
